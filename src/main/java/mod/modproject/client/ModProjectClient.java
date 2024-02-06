ef1f965e2128cf96733dac92ba3cf8e70c347342
NewAmazingPVP <gamingofpat@gmail.com>
2024-02-06 17:20:44 -0500

    sound play

--- src/main/java/mod/modproject/client/ModProjectClient.java
+++ src/main/java/mod/modproject/client/ModProjectClient.java
@@ -8,11 +8,14 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModProjectClient implements ClientModInitializer {
-    public static final Item CUSTOM_ITEM = new Item(new FabricItemSettings());
+
+    public static final CustomItem CUSTOM_ITEM = new CustomItem(new FabricItemSettings());
     public static final Item GEM = new Item((new FabricItemSettings()));
     @Override
     public void onInitializeClient() {
         Registry.register(Registries.ITEM, new Identifier("tutorial", "custom_item"), CUSTOM_ITEM);
         Registry.register(Registries.ITEM, new Identifier("gem", "gem_t1"), GEM);
     }
+
+
 }
