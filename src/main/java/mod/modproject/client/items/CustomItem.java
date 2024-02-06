f71db0a9587b6957111eb8246ca7003fba7868e7
NewAmazingPVP <gamingofpat@gmail.com>
2024-02-06 17:37:17 -0500

    Update CustomItem.java

--- src/main/java/mod/modproject/client/items/CustomItem.java
+++ src/main/java/mod/modproject/client/items/CustomItem.java
@@ -4,6 +4,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
 import net.minecraft.util.Colors;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
@@ -18,7 +19,7 @@ public class CustomItem extends Item {
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
         playerEntity.playSound(SoundEvents.BLOCK_WOOL_BREAK, 1.0F, 1.0F);
-        playerEntity.sendMessage("test",true);
+        playerEntity.sendMessage(Text.of("test"),true);
         return TypedActionResult.success(playerEntity.getStackInHand(hand));
     }
 }
