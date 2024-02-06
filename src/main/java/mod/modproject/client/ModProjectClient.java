47b58f55e08a975a981448528292680cfbab28c4
Comet991 <139285587+Comet991@users.noreply.github.com>
2024-02-06 17:16:14 -0500

    hello =D

--- src/main/java/mod/modproject/client/ModProjectClient.java
+++ src/main/java/mod/modproject/client/ModProjectClient.java
@@ -9,8 +9,10 @@ import net.minecraft.util.Identifier;
 
 public class ModProjectClient implements ClientModInitializer {
     public static final Item CUSTOM_ITEM = new Item(new FabricItemSettings());
+    public static final Item GEM = new Item((new FabricItemSettings()));
     @Override
     public void onInitializeClient() {
         Registry.register(Registries.ITEM, new Identifier("tutorial", "custom_item"), CUSTOM_ITEM);
+        Registry.register(Registries.ITEM, new Identifier("gem", "gem_t1"), GEM);
     }
 }
