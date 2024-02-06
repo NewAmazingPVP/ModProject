142633ecdc831973df7a46ab9915ca4a1434be48
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2024-02-06 16:27:08 -0500

    WIP Fabric MOD
    WIP Fabric MOD

---
+++ src/main/java/mod/modproject/ModProject.java
@@ -0,0 +1,13 @@
+package mod.modproject;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ModProject implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/mod/modproject/client/ModProjectClient.java
@@ -0,0 +1,12 @@
+package mod.modproject.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModProjectClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+    }
+}
